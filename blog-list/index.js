6a5eb701bebff37c16b02d5e93b12abd431bc7ea
wicp <noneof@yourbusiness.com>
2021-04-18 15:00:35 +0100

    extracted routing to controllers/blogs.js

--- blog-list/index.js
+++ blog-list/index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const config = require("./utils/config")
-const Blog = require("./models/blog")
+const blogRouter = require("./controllers/blogs")
 
 const mongoUrl = `${config.MONGO_URL}/bloglist`
 mongoose.connect(mongoUrl, {
@@ -15,20 +15,7 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors())
 app.use(express.json())
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use("/api/blogs", blogRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
