47c76db3cb78c0197c31b0d23fb0d46e967b193c
wicp <noneof@yourbusiness.com>
2021-04-18 14:45:09 +0100

    extracted blog model to models/blog.js

--- blog-list/index.js
+++ blog-list/index.js
@@ -3,15 +3,7 @@ const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const config = require("./utils/config")
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
+const Blog = require("./models/blog")
 
 const mongoUrl = `${config.MONGO_URL}/bloglist`
 mongoose.connect(mongoUrl, {
