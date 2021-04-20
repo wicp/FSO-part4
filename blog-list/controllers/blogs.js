f12525aaf2febb9a9f0fc2acf3f1503d257bc3e7
wicp <noneof@yourbusiness.com>
2021-04-20 15:37:10 +0100

    refactored from try/catch to use express-async-errors

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,23 +1,20 @@
 const blogRouter = require("express").Router()
 const Blog = require("../models/blog")
 
-blogRouter.get("/", async (request, response, next) => {
-  try {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-  } catch (error) {
-    next(error)
-  }
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post("/", async (request, response, next) => {
-  try {
-    const blog = new Blog(request.body)
-    const result = await blog.save()
-    response.status(201).json(result)
-  } catch (error) {
-    next(error)
-  }
+blogRouter.post("/", async (request, response) => {
+  const blog = new Blog(request.body)
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
