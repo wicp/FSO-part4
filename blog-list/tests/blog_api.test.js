83e23b37f659a38625982856379bf033d1e6a47f
wicp <noneof@yourbusiness.com>
2021-04-20 11:05:38 +0100

    completed 4.11

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -56,7 +56,30 @@ test("New blogs are saved correctly", async () => {
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/)
-  //response from Post contains new object
+  //response from Post matches new blog
+  expect(postResponse.body).toMatchObject(newBlog)
+
+  //Get of all blogs now contains new blog
+  const getResponse = await api.get("/api/blogs")
+  expect(getResponse.body).toHaveLength(testData.length + 1)
+  expect(getResponse.body[testData.length]).toMatchObject(newBlog)
+})
+
+test("Missing Likes field defaults to 0", async () => {
+  const newBlog = {
+    title: "New blog test data",
+    author: "Jest",
+    url: "localhost",
+  }
+
+  const postResponse = await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/)
+  
+  newBlog.likes = 0 // setup new Blog to match expected response
+  //response from Post matches new blog
   expect(postResponse.body).toMatchObject(newBlog)
 
   //Get of all blogs now contains new blog
