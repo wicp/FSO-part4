d759e2122538adbc3c8e7b04eb032939bdb1f24d
wicp <noneof@yourbusiness.com>
2021-04-26 17:44:57 +0100

    created User model

---
+++ blog-list/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+})
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model("User", userSchema)
